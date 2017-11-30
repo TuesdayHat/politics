12fc1567645d3d802f7722c3fd02c4bcf492e132
Adam Calhoun and julia sheremet <adam@>
2017-11-30 11:07:38 -0800

    fix adding/scope bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
-  var score = 5;
+  $("form#meter").submit(function(event){
+    event.preventDefault();
+    //debugger;
+    var score = 5;
+    var t = parseInt($("#ttttt").val());
+    var i = parseInt($("#iiiii").val());
+    var lorem = parseInt($("#Lorem").val());
+    var ipsum = parseInt($("#Ipsum").val());
+    var a = parseInt($("#aaaaa").val());
 
-  if (score <=7 && score >= 3){
-    alert("You are  Moderate!")
-  } else if (score <3) {
-    alert("You're an Ipsum!")
-  } else {
-    alert("You're a Lorem!")
-  }
+    function addScore(){
+      return (score + lorem + ipsum + i + t + a);
+    }
+
+    var result = addScore();
+    //console.log("result before branching: " +result);
+
+    if (result <= 7 && result >= 3){
+      alert("You are  Moderate!")
+    } else if (result <3) {
+      alert("You're an Ipsum!")
+    } else if (result > 7){
+      alert("You're a Lorem!")
+    }
+    else {
+      //console.log("else clause");
+    }
+  });
+  //alert(a);
 });
+
+//-------------------------
