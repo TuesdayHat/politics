28b644d2f26355a7828eb27c786cf3633c5a9410
Adam Calhoun and julia sheremet <julia@>
2017-11-30 09:33:50 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  var score = 5;
+
+  if (score <=7 && score >= 3){
+    alert("You are  Moderate!")
+  } else if (score <3) {
+    alert("You're an Ipsum!")
+  } else {
+    alert("You're a Lorem!")
+  }
+});
